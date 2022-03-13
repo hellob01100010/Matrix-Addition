9c13b622238804d4dd697da4f643b750ab5e49c3
scriptpi01 <88918870+scriptpi01@users.noreply.github.com>
2022-03-13 15:33:08 +0700

    Add files via upload

---
+++ Matrix Addition1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int e ,f;
+    cin >> e >> f;
+    int a[e][f];
+    int b[e][f];
+    int c[e][f];
+    for (int i = 0; i < e; i++) {
+        for (int j = 0; j < f; j++) {
+            cin >> a[i][j];
+        }
+    }
+  
+    for (int i = 0; i < e; i++) {
+        for (int j = 0; j < f; j++) {
+            cin >> b[i][j];
+        }
+    }
+    
+    for (int i = 0; i < e; i++) {
+        for (int j = 0; j < f; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+ 
+    cout << endl;
+    return 0;
+}
